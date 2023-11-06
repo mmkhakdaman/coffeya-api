c65cb7d5cb0bbad7e4ab4ecf48e7cea5a09d8093
mahdi khakdadman <khakdamanmohammadmahdi@gmail.com>
2023-11-06 14:10:09 +0330

    add domain resource

--- Modules/Tenant/Transformers/DomainResource.php
+++ Modules/Tenant/Transformers/DomainResource.php
@@ -2,23 +2,22 @@
 
 namespace Modules\Tenant\Transformers;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TenantResource extends JsonResource
+class DomainResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param Request $request
+     * @param \Illuminate\Http\Request
      * @return array
      */
-    public function toArray(Request $request)
+    public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'name' => $this->name,
-            'domain' => DomainResource::make($this->whenLoaded('domain')),
+            'domain' => $this->domain,
+            'tenant_id' => $this->tenant_id,
         ];
     }
 }
